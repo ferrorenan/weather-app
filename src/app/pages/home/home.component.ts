e2de42b6488dbb59e8709823a292fc12b239f8c8
Renan Ferro <renan.ferro@fmd.ag>
2023-05-03 22:46:43 -0300

    feat(structure): implement home structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LocalizationService} from "../../services/localization.service";
+import {WeatherService} from "../../services/weather.service";
+import {Observable} from "rxjs";
+import {WeatherResponsePayload} from "../../models/weather-response-payload";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  private clientLongitute: string = '';
+  private clientLatitude: string = '';
+
+  disableGetWeatherNewsButton: boolean = true;
+
+  weatherData$: Observable<WeatherResponsePayload>;
 
   constructor(
-    private locationService: LocalizationService
+    private locationService: LocalizationService,
+    private _weatherApiService: WeatherService
   ) {}
 
-  getLocalizaionClientLatAndAlt(): void {
+  ngOnInit(): void {
 
-    this.locationService.getPosition().then(pos=>
-    {
-      console.log(`Positon: ${pos.lng} ${pos.lat}`);
-    });
+    this.getClientLatAndAlt();
+  }
+
+  getClientLatAndAlt(): void {
+
+    this.locationService.getPosition()
+      .then(pos => {
+
+        this.clientLatitude = pos.lat;
+        this.clientLongitute = pos.lng;
+
+        console.log(`Positon: ${pos.lng} ${pos.lat}`);
+      })
+      .finally(() => {
+        console.log('Was finalized');
+        this.disableGetWeatherNewsButton = false;
+      });
+  }
+
+  getClientWeatherData(): void {
+
+    this.weatherData$ = this._weatherApiService
+      .getWeatherData(this.clientLatitude, this.clientLongitute);
   }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,19 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HomeComponent} from './home.component';
 import {ImageComponent} from "../../components/image/image.component";
+import {CardWeatherDataComponent} from "../../components/card-weather-data/card-weather-data.component";
+import {CardLocalizationComponent} from "../../components/card-localization/card-localization.component";
+import {WeatherService} from "../../services/weather.service";
 
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
-    imports: [
-        CommonModule,
-        HomeRoutingModule,
-        ImageComponent
-    ]
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ImageComponent,
+    CardWeatherDataComponent,
+    CardLocalizationComponent
+  ],
+  providers: [
+    WeatherService
+  ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
