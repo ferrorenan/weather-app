a4fbd189de9643592f298ccf14e304ab8aea0f53
Renan Ferro <renan.ferro@fmd.ag>
2023-05-08 20:42:51 -0300

    feat(structure): implement icon component structure

---
+++ src/app/components/icon/icon.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+  standalone: true
+})
+export class IconComponent {
+
+  @Input() pathImage: string;
+  @Input() imageClass: string;
+  @Input() width: number = 1;
+  @Input() height: number = 1;
+}
