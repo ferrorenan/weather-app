a12af9e5272ab5142c532baa5e16d0b78af581ef
Renan Ferro <renan.ferro@fmd.ag>
2023-05-16 21:16:38 -0300

    feat(app description): insert app description

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   url_api: 'https://api.openweathermap.org/data/2.5/',
   apiTokenKey: 'fd54aac3963ad841c350f4cb876dd6fa',
+  applicationDescription: 'Get general information of your local weather with just one click!'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   url_api: 'https://api.openweathermap.org/data/2.5/',
   apiTokenKey: 'fd54aac3963ad841c350f4cb876dd6fa',
+  applicationDescription: 'Get general information of your local weather with just one click!'
 }
